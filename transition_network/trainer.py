910d8e8495e096a2e813751bd32248497d2ad1bc
mritv <mritvik@ymail.com>
2020-01-31 10:59:06 +0100

    Trainer changed complying to the new edflow dev branch.

--- transition_network/trainer.py
+++ transition_network/trainer.py
@@ -1,227 +1,104 @@
-import numpy as np
 import torch
-from edflow.custom_logging import ProjectManager
-from edflow.eval.pipeline import EvalHook
-from edflow.hooks.pytorch_hooks import PyCheckpointHook
-from edflow.hooks.pytorch_hooks import PyLoggingHook
-from edflow.hooks.pytorch_hooks import ToFromTorchHook
-from edflow.hooks.util_hooks import IntervalHook
-from edflow.iterators.model_iterator import PyHookedModelIterator
-from edflow.util import walk
+from edflow.iterators.template_iterator import TemplateIterator
+from edflow.util import retrieve
 
 from .triplet_loss import TripletLoss
+from .utils import prepare_logs, prepare_input
 
 
-class TransitionTrainer(PyHookedModelIterator):
-    """Can train and eval :class:`ABC_Net`."""
+class TransitionTrainer(TemplateIterator):
+    """Trainer class to train the network prediction transition probabilities in latent space."""
 
-    def __init__(self, config, root, model, **kwargs):
-        super().__init__(config, root, model, **kwargs)
-
-        self.config = config
-        self.root = root
+    def __init__(self, config, root, model, datasets, **kwargs):
+        super().__init__(config, root, model, datasets, **kwargs)
         self.model = model
 
-        # Mode. What do you want to do?
-        self.mode = config.get("mode", "train")
-
-        self.tb_log_freq = config.get("tb_log_freq", 100)
-        self.log_freq = config.get("log_freq", 1000)
-
-        triplet_margin = config.get("triplet_margin", 0.4)
+        triplet_margin = config.get("triplet_margin", 0.3)
         self.triplet_loss = TripletLoss(triplet_margin)
+        self.bce_loss = torch.nn.BCELoss()
         self.bs = config["batch_size"]
 
-        # Training stuff
-        if self.mode == "train":
-            self.lr = lr = config.get("lr", 1e-6)
-            beta_1 = config.get("beta_1", 0.5)
-            beta_2 = config.get("beta_2", 0.9)
+        self.lr = retrieve(config, "optimizer/lr", default=1e-4)
+        beta_1 = retrieve(config, "optimizer/beta_1", default=0.5)
+        beta_2 = retrieve(config, "optimizer/beta_2", default=0.99)
 
-            self.optimizer = torch.optim.Adam(
-                model.parameters(), lr=lr, betas=(beta_1, beta_2)
-            )
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=self.lr, betas=(beta_1, beta_2))
 
-            # Initialize losses
-            self.loss_weights = config.get(
-                "loss_weights",
-                {"kl": 0.01, "rec": 5, "reg": 0.1, "fp": 0.01, "app_enc": 0.0},
-            )
+    def save(self, path):
+        self.model.cpu()
+        torch.save(self.model.state_dict(), path)
+        if torch.cuda.is_available():
+            self.model.cuda()
 
-        DPrepH = DataPrepHook()
+    def restore(self, checkpoint_path):
+        self.model.cpu()
+        self.model.load_state_dict(torch.load(checkpoint_path, map_location="cpu"))
+        if torch.cuda.is_available():
+            self.model.cuda()
 
-        # Logging scalars. Check if they are also in log_dict.
-        scalars = [
-            "step_ops/losses/triplet_loss",
-        ]
+    def initialize(self, *args, **kwargs):
+        super().initialize(*args, **kwargs)
+        if torch.cuda.is_available():
+            self.model.cuda()
 
-        def get_inputs(
-            model,
-            z_anchor,
-            z_positive,
-            z_negative,
-            *args,
-            **kwargs,
-        ):
-            input_dict = {
-                "inputs": {
-                    "z_anchor": z_anchor,
-                    "z_positive": z_positive,
-                    "z_negative": z_negative,
-                }
-            }
-            return input_dict
+    def step_op(self, model, **input_dict) -> dict:
+        """
+        Main step function required by the edflow framework. Loads the data and model, and performs the forward pass.
+        :param model: Edflow model class
+        :param input_dict: Dictionary returned by the edflow training or validation dataset.
+        :return: Returns a dictionary containing the train_op, eval_op and log_op functions.
+        """
+        scalars = dict()
+        prepare_input(input_dict)
+        z_anchor = input_dict["z_anchor"]
+        z_positive = input_dict["z_positive"]
+        z_negative_1 = input_dict["z_negative_1"]
+        z_negative_2 = input_dict["z_negative_2"]
 
-        loghook = PyLoggingHook(
-            log_ops=[get_inputs],
-            scalar_keys=scalars,
-            root_path=ProjectManager.train,
-            interval=self.tb_log_freq,
-        )
-
-        ckpt_hook = PyCheckpointHook(
-            root_path=ProjectManager.checkpoints,
-            model=model,
-            modelname="transition_net",
-            interval=self.log_freq,
-        )
-
-        interval = IntervalHook(
-            [loghook, ckpt_hook],
-            1,
-            modify_each=1,
-            modifier=lambda x: x,
-            max_interval=1000,
-            get_step=self.get_global_step,
-        )
-
-        if self.mode == "train":
-            self.hooks += [DPrepH, ckpt_hook, interval]  # ,tb_interval]
-        elif self.mode == "eval" or self.mode == "test":
-            from edflow.main import get_implementations_from_config
-
-            eval_dataset = get_implementations_from_config(self.config, ["dataset"])[
-                "dataset"
-            ](config)
-
-            labels_key = "step_ops/labels"
-            eval_hook = EvalHook(
-                dataset=eval_dataset,
-                labels_key=labels_key,
-                callbacks={},
-                config=config,
-                step_getter=self.get_global_step,
-            )
-
-            self.hooks += [DPrepH, eval_hook]
-
-    def initialize(self, checkpoint_path: str = None, **kwargs):
-        if checkpoint_path is not None:
-            pretrained_state_dict = torch.load(checkpoint_path)
-            self.model.load_state_dict(pretrained_state_dict)
-            self.logger.info(f"Found checkpoint at {checkpoint_path}.")
-        self.model.cuda()
-
-    def train_op(
-        self,
-        model,
-        z_anchor,
-        z_positive,
-        z_negative,
-        **kwargs,
-    ):
+        pos_dis = input_dict["pos_dis"]
+        neg_dis_1 = input_dict["neg_dis_1"]
+        neg_dis_2 = input_dict["neg_dis_2"]
         # Setting up the scalar logs dictionary
-        loss_dict = {
-            "triplet_loss": [],
-        }
-        log_dict = {"lr": self.lr}
-
+        scalars["negative_distance_min"] = neg_dis_1[0]
+        scalars["negative_distance_max"] = neg_dis_2[0]
+        scalars["positive_distance"] = pos_dis[0]
         # Initialize Optimizer
-        self.optimizer.zero_grad()
-        positive_ex = torch.cat((z_anchor, z_positive), dim=1).squeeze(2).squeeze(2)
-        neg_ex = torch.cat((z_anchor, z_negative), dim=1).squeeze(2).squeeze(2)
+        positive_ex = torch.cat((z_anchor, z_positive), dim=3).squeeze()
+        neg_ex_1 = torch.cat((z_anchor, z_negative_1), dim=3).squeeze()
+        neg_ex_2 = torch.cat((z_anchor, z_negative_2), dim=3).squeeze()
+
         positive_pass = model(positive_ex)
-        negative_pass = model(neg_ex)
-
-        triplet_loss = self.triplet_loss(anchor=torch.zeros_like(positive_ex),
-                                         pos=positive_pass,
-                                         neg=negative_pass)
-        if self.get_global_step() % 1000 == 0:
-            print("LOSS:------", triplet_loss)
-            print("POS:-------", positive_pass)
-            print("NEG:-------", negative_pass)
-        loss_dict["triplet_loss"] += [triplet_loss]
-
-        triplet_loss.backward()
-        self.optimizer.step()
-
-        for k, v in loss_dict.items():
-            if len(v) == 1:
-                loss_dict[k] = v[0]
-            else:
-                t_loss = 0
-                for t in v:
-                    t_loss += t
-                loss_dict[k] = t_loss
-
-        return {"losses": loss_dict, "other": log_dict}
-
-    def eval_op(
-        self,
-        model,
-        z_concatenated,
-        **kwargs,
-    ):
-        """
-        :param model:
-        :param z_concatenated:
-        :param kwargs:
-        :return:
-        """
-        full_batch_probabilities = torch.zeros(self.bs, z_concatenated.shape[1], 1)
-        for i, batch in enumerate(z_concatenated):
-            probabilities = model(batch.squeeze(2).squeeze(2))
-            full_batch_probabilities[i] = probabilities.squeeze(-1)
-        return {"labels": {"transition_matrix": full_batch_probabilities}}
-
-    def step_ops(self):
-        if self.config.get("test_mode", False):
-            return self.eval_op
+        negative_pass_1 = model(neg_ex_1)
+        negative_pass_2 = model(neg_ex_2)
+        if self.config.get("loss") == "triplet":
+            loss = self.triplet_loss(
+                anchor=torch.zeros_like(positive_ex), pos=positive_pass, neg=negative_pass_1
+            ) + self.triplet_loss(
+                anchor=torch.zeros_like(positive_ex), pos=positive_pass, neg=negative_pass_2
+            )
+        elif self.config.get("loss") == "bce":
+            loss = self.bce_loss(positive_pass, torch.ones_like(positive_pass)) + 0.5 * (
+                self.bce_loss(negative_pass_1, torch.ones_like(negative_pass_1))
+                + self.bce_loss(negative_pass_2, torch.zeros_like(negative_pass_2))
+            )
         else:
-            return self.train_op
+            raise ValueError("Unknown loss function.")
+        scalars["loss"] = loss
 
+        def train_op():
+            """ Backward gradient prop only if training """
+            scalars["lr"] = self.lr
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
 
-class DataPrepHook(ToFromTorchHook):
-    """Do not only convert to and from torch tensor, but also make sure
-    image dimensions are correct.
-    """
+        def log_op():
+            """ Return scalars needed to be logged"""
+            prepare_logs(scalars)
+            return {"scalars": scalars}
 
-    def before_step(self, step, fetches, feeds, batch):
-        """ Before converting: transpose to ``[B, C, H, W]``."""
+        def eval_op():
+            """ Return nothing as of now"""
+            return {}
 
-        def to_image(obj):
-            if isinstance(obj, np.ndarray) and len(obj.shape) == 4:
-                return obj.transpose(0, 3, 1, 2)
-            else:
-                return obj
-
-        walk(feeds, to_image, inplace=True)
-
-        super().before_step(step, fetches, feeds, batch)
-
-    def after_step(self, step, results):
-        """After converting: transpose to ``[B, H, W, C]``."""
-
-        super().after_step(step, results)
-
-        def to_image(k, obj):
-            if (
-                "weights" not in k
-                and isinstance(obj, np.ndarray)
-                and len(obj.shape) == 4
-            ):
-                return obj.transpose(0, 2, 3, 1)
-            else:
-                return obj
-
-        walk(results, to_image, inplace=True, pass_key=True)
+        return {"train_op": train_op, "log_op": log_op, "eval_op": eval_op}
