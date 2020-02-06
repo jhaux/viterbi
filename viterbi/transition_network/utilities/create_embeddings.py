017bf9c8538c075e862b3b83cf0fa4158c9878a7
Johannes Haux <jo.mobile.2011@gmail.com>
2020-02-06 17:46:46 +0100

    :tada: create_embeddings with batches of data

--- viterbi/transition_network/utilities/create_embeddings.py
+++ viterbi/transition_network/utilities/create_embeddings.py
@@ -25,8 +25,14 @@ def compute_transition_probability(
     :param transition_model: A trained model for computing the transition probability between two embeddings.
     :return: The transition probability as a torch.Tensor.
     """
-    pose_embedding_1 = create_embedding(keypoints_1, interpolation_model).unsqueeze(0)
-    pose_embedding_2 = create_embedding(keypoints_2, interpolation_model).unsqueeze(0)
+    pose_embedding_1 = create_embedding(keypoints_1, interpolation_model)
+    pose_embedding_2 = create_embedding(keypoints_2, interpolation_model)
+
+    if len(pose_embedding_1.shape) == 1:
+        pose_embedding_1.unsqueeze(0)
+    if len(pose_embedding_2.shape) == 1:
+        pose_embedding_2.unsqueeze(0)
+
     return transition_model(torch.cat((pose_embedding_1, pose_embedding_2), dim=1))
 
 
@@ -37,8 +43,12 @@ def create_embedding(keypoints: np.ndarray, model: nn.Module) -> torch.Tensor:
     :param model: A trained model with the method `hm_head` which creates an embedding of the pose.
     :return: The pose embedding as a torch.Tensor.
     """
-    keypoints_tensor = torch.tensor(keypoints).cuda()
-    heatmap = kp2heat(keypoints_tensor).unsqueeze(0).float()
+    keypoints_tensor = torch.tensor(keypoints)
+    if torch.cuda.is_available:
+        keypoints_tensor = keypoints_tensor.cuda()
+    heatmap = kp2heat(keypoints_tensor).float()
+    if len(heatmap.shape) == 3:
+        heatmap = heatmap.unsqueeze(0)
     return model.hm_head(heatmap).squeeze()
 
 
@@ -48,7 +58,7 @@ def load_interpolation_model(model_path: str) -> nn.Module:
     :param model_path: Path to the checkpoint file.
     :return: Model with the trained weights.
     """
-    config = {"heatmap_input_channels": 6, "input_channels": 3}
+    config = {"heatmap_input_channels": 6, "input_channels": 3, 'add_out_channels': 1}
     model = BasicNet(config)
     initialize_model(model, model_path)
     return model
@@ -79,9 +89,27 @@ def initialize_model(model: nn.Module, model_path: str) -> nn.Module:
         model.cuda()
 
 
+class TransProb:
+    def __init__(self):
+        interpolation_checkpoint_path = "/export/data/rmarwaha/projects/logs/2019-11-20T14-46-23_hg_disc/train/checkpoints/1498-100005_abc_net.ckpt"
+        transition_checkpoint_path = '/export/home/rmarwaha/projects/viterbi/logs/2020-02-03T14-08-26_transistion_network_bce/train/checkpoints/model-71833.ckpt'
+        # transition_checkpoint_path = "/export/home/rmarwaha/projects/logs/2020-01-31T10-45-03_transistion_network_triplet/train/checkpoints/model-71833.ckpt"
+
+        self.interpolation_model = load_interpolation_model(interpolation_checkpoint_path)
+        self.transition_model = load_transition_model(transition_checkpoint_path)
+
+    def __call__(self, kp1, kp2):
+        return compute_transition_probability(
+                kp1, kp2,
+                self.interpolation_model, self.transition_model
+                )
+
+
 if __name__ == '__main__':
     interpolation_checkpoint_path = "/export/data/rmarwaha/projects/logs/2019-11-20T14-46-23_hg_disc/train/checkpoints/1498-100005_abc_net.ckpt"
-    transition_checkpoint_path = "/export/home/rmarwaha/projects/logs/2020-01-31T10-45-03_transistion_network_triplet/train/checkpoints/model-71833.ckpt"
+
+    transition_checkpoint_path = '/export/home/rmarwaha/projects/viterbi/logs/2020-02-03T14-08-26_transistion_network_bce/train/checkpoints/model-71833.ckpt'
+    # transition_checkpoint_path = "/export/home/rmarwaha/projects/logs/2020-01-31T10-45-03_transistion_network_triplet/train/checkpoints/model-71833.ckpt"
     interpolation_model = load_interpolation_model(interpolation_checkpoint_path)
     transition_model = load_transition_model(transition_checkpoint_path)
     from abc_interpolation.datasets.human_gait.human_gait import HumanGait_abc
@@ -93,3 +121,5 @@ if __name__ == '__main__':
     trans_prob = compute_transition_probability(kp_1, kp_2, interpolation_model, transition_model)
     print(trans_prob)
 
+    TP = TransProb()
+    print(TP(kp_1, kp_2))
